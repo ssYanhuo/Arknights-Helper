30940d72de9b83cd81a33b52ffbff4570499bb59
ssYanhuo <unconfigured@null.spigotmc.org>
2019-07-04 13:38:29 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Arknights-Helper
+ Arknights helper mobile app.
---
+++ app/src/main/java/com/ssyanhuo/arknightshelper/BackendService.java
@@ -0,0 +1,22 @@
+package com.ssyanhuo.arknightshelper;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BackendService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
