f2e59d6b2e3c1277fa5db10a269712137e14b792
三生烟火 <44744968+ssYanhuo@users.noreply.github.com>
2022-03-02 10:03:40 +0800

    Data update.

--- app/src/main/java/com/ssyanhuo/arknightshelper/activity/MainActivity.java
+++ app/src/main/java/com/ssyanhuo/arknightshelper/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
